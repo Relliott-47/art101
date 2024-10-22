8da49e5c296d72a14f21f43b7f2c07fc5f5e42c1
Relliott-47 <Rileyelliott47@gmail.com>
2024-10-22 15:41:44 -0700

    Update index.html

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+ *Author: Riley ELliott
+ *Created: 21 October 2024
+ *License: Public Domain
+ */
+
+// Define Variables
+make = "Ford";
+model = "Escape";
+color = "Green";
+year = 2006;
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
